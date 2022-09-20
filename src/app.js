582189107a1e7858ffc7e1c2cf6c4585aed85344
alex <osenchyn@gmail.com>
2022-09-20 03:17:28 +0300

    preparing to deploy

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const PORT = 5000 || 3000 
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+    res.render('index')
+})
+app.listen(PORT, ()=> {
+    console.log('http://localhost:' + PORT + '/')
+})
+
+
